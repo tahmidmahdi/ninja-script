10076b3b19b7a9083993607bc83b696c9e673b29
Tahmid <tahmid5550@gmail.com>
2022-07-16 16:24:03 +0600

    Head root dirr renamed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/Head';
+import Head from 'next/head';
 import Link from "next/link";
 import styles from '../styles/Home.module.css';
 export default function Home() {
--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -27,7 +27,7 @@ const Details = ({ ninja }) => {
     return (
         <div>
             <h1>{ninja?.name}</h1>
-            <p>{ninja.email}</p>
+            <p>{ninja.email}</p> 
             <p>{ninja?.website}</p>
             <p>{ninja?.address.city}</p>
         </div>
