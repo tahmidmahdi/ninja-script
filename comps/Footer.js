b81f27bff75174ae5878ba79234945b80bf886e6
Tahmid <tahmid5550@gmail.com>
2022-07-16 14:16:45 +0600

    Hot Modules, code split

---
+++ comps/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            Copyright {new Date().getFullYear()} Ninja List
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const about = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, iure.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, iure.</p>
+        </div>
+    );
+};
+
+export default about;
